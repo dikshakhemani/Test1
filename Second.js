f9b90035ac34ff453df0705c67591c02f4ce6d80
dikshakhemani <khdiksha18@gmail.com>
2024-07-25 23:24:09 +0530

    Second Program

---
+++ Second.js
@@ -0,0 +1,14 @@
+let x=55;
+let y=21;
+let max;
+if(x>y){
+    max=x;
+}
+else{
+    max=y;
+}
+console.log("Maximum of the two is "+max);
+
+//using ternary operator
+let maxi=(x>y)?x:y;
+console.log("Maximum of the two is "+maxi);
